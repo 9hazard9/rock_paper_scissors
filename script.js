c63b6f7dbd9808bb40aeeb1431791ab0590c9eb0
9hazard9 <160774980+9hazard9@users.noreply.github.com>
2024-03-21 17:05:18 -0500

    Add event listeners to buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    const computerChoices = ['rock', 'paper', 'scissors'];
+    const computerChoices = ['Rock', 'Paper', 'Scissors'];
     // Math.random() gets random number between(0-1, 1 exclusive)
     // The number from Math.random() is multiplied by the array length to get the numbers between(0-3)
     // Math.floor() gets the index ranging from(0 to arrayLength-1)
@@ -8,18 +8,16 @@ function getComputerChoice() {
 }
 
 function displayScoreTable(playerPoints, computerPoints) {
-    return console.log(`||Player: ${playerPoints}||\t\t||Computer: ${computerPoints}||`);
+    return console.log(`\n||Player: ${playerPoints}||\t\t||Computer: ${computerPoints}||\n`);
 }
 
 function playGame() {
     function playRound(playerSelection, computerSelection) {
         switch(playerSelection) {
-            case 'rock':
-                if (computerSelection === 'rock') {
+            case 'Rock':
+                if (computerSelection === 'Rock') {
                     console.log("Rock vs Rock. It's a tie!");
-                    playerPoints++;
-                    computerPoints++;
-                } else if (computerSelection === 'paper') {
+                } else if (computerSelection === 'Paper') {
                     console.log("Rock vs Paper. Sorry, you lose!");
                     computerPoints++;
                 } else {
@@ -28,25 +26,24 @@ function playGame() {
                 }
                 displayScoreTable(playerPoints, computerPoints);
                 break;
-            case 'paper':
-                if (computerSelection === 'rock') {
+            case 'Paper':
+                if (computerSelection === 'Rock') {
                     console.log("Paper vs Rock. Your a winner! Yay!");
                     playerPoints++;
-                } else if (computerSelection === 'paper') {
+                } else if (computerSelection === 'Paper') {
                     console.log("Paper vs Paper. It's a tie!");
                     playerPoints++;
-                    computerPoints++;
                 } else {
                     console.log("Paper vs Scissors. You lose! Bettter luck next time!");
                     computerPoints++;
                 }
                 displayScoreTable(playerPoints, computerPoints);
                 break;
-            case 'scissors':
-                if (computerSelection === 'rock') {
+            case 'Scissors':
+                if (computerSelection === 'Rock') {
                     console.log("Scissors vs Rock. Oh no, you lose! Don't give up!");
                     computerPoints++;
-                } else if(computerSelection === 'paper') {
+                } else if(computerSelection === 'Paper') {
                     console.log("Scissors vs Paper. Congratulations, you win!");
                     playerPoints++;
                 } else {
@@ -59,49 +56,16 @@ function playGame() {
         }
     }
 
-    // Variable breaks out of while loop
-    let continueGame = true;
-
-    // Variables to keep track of score
     let playerPoints = 0;
     let computerPoints = 0;
 
-    // Loop continues to initiate new rounds until player quits
-    while(continueGame) {
-        // Variables to break out of while loops
-        let validChoice = true;
-        let validContinue = true;
+    const allButtons = document.querySelectorAll(".button");
 
-        // Initializes variable for player input
-        let playerInput;
-
-        // Loop checks for valid player input
-        while(validChoice) {
-            // Accepts player input and sets it to lower case
-            playerInput = prompt("Enter your choice ('rock', 'paper', 'scissors'): ").toLowerCase();
-            // The loop will continue until a valid input is provided
-            (!((playerInput == 'rock') || (playerInput == 'paper') || (playerInput == 'scissors'))) ? alert("\n\nEnter a valid value!\n\n")
-            : validChoice = false;
-        }
-
-        // Calls playRound function
-        playRound(playerInput, getComputerChoice());
-
-        // Loop checks for valid player input
-        while(validContinue) {
-             // Asks player if they wish to continue
-            let continueInput = prompt("Would you like to play again ('y' or 'n')?:  ").toLowerCase();
-            // Returns an alert if an invalid input is entered, else break the loop
-            if(!((continueInput == 'y') || (continueInput == 'n'))) {
-                alert("Enter a valid input ('y' or 'n')!");
-            } else if(continueInput == 'y') {
-                validContinue = false;
-            } else {
-                validContinue = false;
-                continueGame = false;
-            }
-        }
-    }
+    allButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            playRound(event.target.name, getComputerChoice());
+        })
+    });
 }
 
 playGame();
